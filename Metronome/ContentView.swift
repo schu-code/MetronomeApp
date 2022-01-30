ca9b7d25b37e1443c1dd897e9349eb3ec7d7ab73
SC <sophiachu@gmail.com>
2022-01-29 21:33:59 -0800

    Initial Commit

---
+++ Metronome/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Metronome
+//
+//  Created by Sophia Chu on 1/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Metronome/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome
+//
+//  Created by Sophia Chu on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
